b896182942bea5c91e1049082a85807090a17ea9
Felix Häcker <felix@prefix.dev>
2025-08-04 11:14:22 +0200

    fix: Use semicolon instead of colon for PATH

--- recipe/activate.bat
+++ recipe/activate.bat
@@ -1,7 +1,7 @@
 set CARGO_HOME=%CONDA_PREFIX%\.cargo
 set CARGO_CONFIG=%CARGO_HOME%\config
 set RUSTUP_HOME=%CARGO_HOME%\rustup
-set PATH=%CARGO_HOME%\bin:%PATH%
+set PATH=%CARGO_HOME%\bin;%PATH%
 
 if not exist "%CARGO_HOME%" mkdir "%CARGO_HOME%"
 
